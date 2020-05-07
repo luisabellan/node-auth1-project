7daf955adb0c38951c356955df0271eca8e033c3
luisabellan <luis.abellan.dev@gmail.com>
2020-05-07 13:34:15 +0200

    react app added, working on it - save

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+// Helper styles for demo
+
+
+import React from "react";
+import { render } from "react-dom";
+
+
+const App = () => (
+  <div className="app">
+  
+
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
+
+export default App;
